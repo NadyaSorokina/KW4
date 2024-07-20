6c6c35e3a4233e3b236333b9aa75e991186fa331
Sorokina Nadya <zebrakhv@gmail.com>
2024-07-20 20:27:42 +1000

    changed main

--- main.py
+++ main.py
@@ -22,42 +22,47 @@ def main():
     # Загрузка в файл Json
     json_hh_parser = JsonLoadRequests(hh_parser.vacancies, VACANCY_PATH)
     json_hh_parser.write_requests()
-   # vacancy_list = json_hh_parser.json_to_list()
+
     # Загрузка данных из файла Json в список
     vacancy_list = JsonToList(VACANCY_PATH).modification_list()
 
-    # Преобразование загруженного списка в список с валидированными данными
-    vacancy_list_to_list = ListToValidateList(vacancy_list)
-
-    #Сортировка валидированного списка по зп
-    vacancy_sort_list = SortList(vacancy_list_to_list.modification_list())
-
-    #Формирование списка заданной пользователем длины
-    vacancy_count = input("Мы загрузили для Вас список топ-вакансий по зарплате.\n"
-                          "Введите количество вакансий, которые хотите просмотреть:\n")
-    while not vacancy_count.isdigit():
-        vacancy_count = input("Введите количество цифрой:\n")
+    if len(vacancy_list) == 0:
+        print("По Вашему запросу ничего не найдено. Попробуйте заново.")
+        return
     else:
-        print("Вакансии по Вашему запросу")
-        vacancy_crop_list = CropList(vacancy_sort_list.modification_list(), vacancy_count).modification_list()
-        #print(vacancy_crop_list)
 
-        #Формирование и вывод списка объектов класса Vacancy
-        list_class_vacancy = ListVacancyObject(vacancy_crop_list)
-        list_class_vacancy.create_list_class_object()
-        for vacancy in list_class_vacancy.list_object:
-            print(f"{vacancy}\n")
 
-        #Формирование и вывод списка объектов класса Vacancy по ключевому слову в требованиях
-        user_request = input("Введите ключевые слова для поиска по требованиям.\n"
-                            "Несколько ключевых слов вводите через пробел:\n")
-        print("Вакансии по Вашему запросу:")
-        list_class_vacancy.select_list_requirement(user_request)
-        for element in list_class_vacancy.list_select:
-            print(element)
+        # Преобразование загруженного списка в список с валидированными данными
+        vacancy_list_to_list = ListToValidateList(vacancy_list)
 
-    #Очищение JSON файла
-    json_hh_parser.clear_json()
+        #Сортировка валидированного списка по зп
+        vacancy_sort_list = SortList(vacancy_list_to_list.modification_list())
+
+        #Формирование списка заданной пользователем длины
+        vacancy_count = input("Мы загрузили для Вас список топ-вакансий по зарплате.\n"
+                            "Введите количество вакансий, которые хотите просмотреть:\n")
+        while not vacancy_count.isdigit():
+            vacancy_count = input("Введите количество цифрой:\n")
+        else:
+            print("Вакансии по Вашему запросу")
+            vacancy_crop_list = CropList(vacancy_sort_list.modification_list(), vacancy_count).modification_list()
+
+            #Формирование и вывод списка объектов класса Vacancy
+            list_class_vacancy = ListVacancyObject(vacancy_crop_list)
+            list_class_vacancy.create_list_class_object()
+            for vacancy in list_class_vacancy.list_object:
+                print(f"{vacancy}\n")
+
+            #Формирование и вывод списка объектов класса Vacancy по ключевому слову в требованиях
+            user_request = input("Введите ключевые слова для поиска по требованиям.\n"
+                                "Несколько ключевых слов вводите через пробел:\n")
+            print("Вакансии по Вашему запросу:")
+            list_class_vacancy.select_list_requirement(user_request)
+            for element in list_class_vacancy.list_select:
+                print(element)
+
+        #Очищение JSON файла
+        json_hh_parser.clear_json()
 
 
 if __name__ == '__main__':
