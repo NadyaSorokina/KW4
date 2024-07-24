f0216dc9d5bbfa8c5f26d8105a20ccba17dea82e
Sorokina Nadya <zebrakhv@gmail.com>
2024-07-24 18:37:20 +1000

    add vacancy_parser.json change config

--- src/config.py
+++ src/config.py
@@ -3,5 +3,5 @@ import os
 # строим пути для файлов
 ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
 DATA_PATH = os.path.join(ROOT_PATH, 'data')
-VACANCY_PATH = os.path.join(DATA_PATH, 'vacancy_parser')
+VACANCY_PATH = os.path.join(DATA_PATH, 'vacancy_parser.json')
 RESULT_VACANCY_PATH = os.path.join(DATA_PATH, 'vacancy')
